8c3ba4e384154b93bdbb639ac9581f20121ecc5b
Paola de Perthuis <perthuis@di.ens.fr>
2024-01-04 11:44:54 +0100

    rebasing

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,21 +8,10 @@ let toggleTheme = (theme) => {
   }
 };
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8c22196 (Initial commit)
 let setTheme = (theme) => {
   transTheme();
   setHighlight(theme);
   setGiscusTheme(theme);
-<<<<<<< HEAD
-  // if mermaid is not defined, do nothing
-  if (typeof mermaid !== 'undefined') {
-    setMermaidTheme(theme);
-  }
-=======
->>>>>>> 8c22196 (Initial commit)
 
   if (theme) {
     document.documentElement.setAttribute("data-theme", theme);
@@ -67,10 +56,6 @@ let setTheme = (theme) => {
   }
 };
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8c22196 (Initial commit)
 let setHighlight = (theme) => {
   if (theme == "dark") {
     document.getElementById("highlight_theme_light").media = "none";
@@ -81,10 +66,6 @@ let setHighlight = (theme) => {
   }
 };
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8c22196 (Initial commit)
 let setGiscusTheme = (theme) => {
   function sendMessage(message) {
     const iframe = document.querySelector("iframe.giscus-frame");
@@ -99,52 +80,6 @@ let setGiscusTheme = (theme) => {
   });
 };
 
-<<<<<<< HEAD
-
-let addMermaidZoom = (records, observer) => {
-  var svgs = d3.selectAll(".mermaid svg");
-  svgs.each(function () {
-    var svg = d3.select(this);
-    svg.html("<g>" + svg.html() + "</g>");
-    var inner = svg.select("g");
-    var zoom = d3.zoom().on("zoom", function (event) {
-      inner.attr("transform", event.transform);
-    });
-    svg.call(zoom);
-  });
-  observer.disconnect();
-};
-
-
-let setMermaidTheme = (theme) => {
-  if (theme == "light") {
-    // light theme name in mermaid is 'default'
-    // https://mermaid.js.org/config/theming.html#available-themes
-    theme = "default";
-  }
-
-  /* Re-render the SVG, based on https://github.com/cotes2020/jekyll-theme-chirpy/blob/master/_includes/mermaid.html */
-  document.querySelectorAll('.mermaid').forEach((elem) => {
-    // Get the code block content from previous element, since it is the mermaid code itself as defined in Markdown, but it is hidden
-    let svgCode = elem.previousSibling.childNodes[0].innerHTML;
-    elem.removeAttribute('data-processed');
-    elem.innerHTML = svgCode;
-  });
-
-  mermaid.initialize({ theme: theme });
-  window.mermaid.init(undefined, document.querySelectorAll('.mermaid'));
-
-  const observable = document.querySelector(".mermaid svg");
-  if (observable !== null) {
-    var observer = new MutationObserver(addMermaidZoom);
-    const observerOptions = { childList: true };
-    observer.observe(observable, observerOptions);
-  }
-};
-
-
-=======
->>>>>>> 8c22196 (Initial commit)
 let transTheme = () => {
   document.documentElement.classList.add("transition");
   window.setTimeout(() => {
@@ -152,10 +87,6 @@ let transTheme = () => {
   }, 500);
 };
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8c22196 (Initial commit)
 let initTheme = (theme) => {
   if (theme == null || theme == "null") {
     const userPref = window.matchMedia;
@@ -167,18 +98,4 @@ let initTheme = (theme) => {
   setTheme(theme);
 };
 
-<<<<<<< HEAD
-
 initTheme(localStorage.getItem("theme"));
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const mode_toggle = document.getElementById("light-toggle");
-
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
-    });
-});
-=======
-initTheme(localStorage.getItem("theme"));
->>>>>>> 8c22196 (Initial commit)
